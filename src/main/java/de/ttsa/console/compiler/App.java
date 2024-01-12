193f5fc52568d1be19be48b6d8ec16ed9926dc21
Matographo <Matographo@gmail.com>
2024-01-12 22:58:29 +0100

    alle start sachen hinein gepackt

---
+++ src/main/java/de/ttsa/console/compiler/App.java
@@ -0,0 +1,13 @@
+package de.ttsa.console.compiler;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Compiler" );
+    }
+}
