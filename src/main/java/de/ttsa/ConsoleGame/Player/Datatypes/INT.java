0064588a314fd2f955c53d059db6ae98a6a0c423
Matographo <Matographo@gmail.com>
2024-01-20 16:38:49 +0100

    Datentyp INT hinzu gefügt

---
+++ src/main/java/de/ttsa/ConsoleGame/Player/Datatypes/INT.java
@@ -0,0 +1,24 @@
+package de.ttsa.ConsoleGame.Player.Datatypes;
+
+public class INT implements Printablable {
+    
+    private int value;
+
+    public INT(int value) {
+        this.value = value;
+    }
+
+    public void setValue(int value) {
+        this.value = value;
+    }
+
+    public int getValue() {
+        return this.value;
+    }
+
+    @Override
+    public String print() {
+        return value + "";
+    }
+
+}
