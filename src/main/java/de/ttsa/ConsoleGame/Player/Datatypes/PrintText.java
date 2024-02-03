9a92470d12eabf3ea74cc8676d7f5eb5faaf0652
Matographo <Matographo@gmail.com>
2024-02-03 14:55:53 +0100

    Added Documentation to the Datatypes

--- src/main/java/de/ttsa/ConsoleGame/Player/Datatypes/PrintText.java
+++ src/main/java/de/ttsa/ConsoleGame/Player/Datatypes/PrintText.java
@@ -2,11 +2,29 @@ package de.ttsa.ConsoleGame.Player.Datatypes;
 
 public class PrintText implements Printablable{
 
+
+// ---------------------------- Attributes ----------------------------
+
+
+
     private final String text;
+
+
+// --------------------------- Constructor ---------------------------
     
+
+    /**
+     * Constructor for PrintText
+     * @param text the String to print
+     */
     public PrintText(String text) {
         this.text = text;
     }
+
+
+// ----------------------------- Methods -----------------------------
+
+
 
     @Override
     public String print() {
--- src/main/java/de/ttsa/ConsoleGame/Player/Datatypes/Printablable.java
+++ src/main/java/de/ttsa/ConsoleGame/Player/Datatypes/Printablable.java
@@ -2,6 +2,10 @@ package de.ttsa.ConsoleGame.Player.Datatypes;
 
 public interface Printablable {
     
+    /**
+     * Print the value of the datatype
+     * @return the value of the datatype
+     */
     public String print();
     
 }
